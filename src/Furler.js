81981138da1fcc70c0d3e69565420e57d9e580f2
collinsnji <collins.geek@gmail.com>
2017-08-17 22:16:07 -0400

    Throw an exception if lyrics was not found added jsdocs as well. I <3 documentation

--- src/Furler.js
+++ src/Furler.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * Versionizr v0.0.1
+ * Furler v0.0.1
  * 
  * @author Collin Grimm <collin.grimm@protonmail.ch>
  * @copyright (c) 2017 Collin Grimm
@@ -11,24 +11,32 @@
 const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
+const path = require('path');
 const { exec } = require('child_process');
 
 var Spacer = function (str) { return str.toLowerCase().match(/[^_\s\W]+/g).join(''); }
+var lyricsDir = path.join(path.resolve('.'), 'lyrics');
 
+/**
+ * 
+ * @param {String} song Name of song. Default => 'Elastic Heart'
+ * @param {String} artist Name of artist. Default => 'Sia'
+ */
 var Furler = function (song, artist) {
     this.artist = artist || 'Sia';
     this.song = song || 'Elastic Heart';
     return this;
 }
 Furler.prototype.Lyrics = function () {
-    var sia = this;
-    var LyricURL = `https://www.azlyrics.com/lyrics/${Spacer(sia.artist)}/${Spacer(sia.song)}.html`;
-    request(LyricURL, function (error, response, songData) {
+    var Sia = this;
+    var LyricURL = `https://www.azlyrics.com/lyrics/${Spacer(Sia.artist)}/${Spacer(Sia.song)}.html`;
+    return request(LyricURL, function (error, response, songData) {
         var $ = cheerio.load(songData);
-        var lyrics = $('.text-center').children('div').text().toString().replace(/^(\n){2,}/gm, "\r\n");
+        if ($('html').children('body').toString().includes('Welcome to AZLyrics!')) { throw new Error('Song not found'); }
 
-        fs.writeFileSync(`${Spacer(sia.song)}.txt`, lyrics);
-        const child = exec(`./siafy ${Spacer(sia.song)}.txt`);
+        var lyrics = $('.text-center').children('div').text().toString().replace(/^(\n){2,}/gm, "\r\n");
+        fs.writeFileSync(`${lyricsDir}/${Spacer(Sia.song)}.txt`, lyrics);
+        exec(`./siafy ${lyricsDir}/${Spacer(Sia.song)}.txt`);
     });
 }
 
