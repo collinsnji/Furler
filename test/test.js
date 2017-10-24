87c2884735515e8b739688847eed1d53dbad6fef
collinsnji <collins.geek@gmail.com>
2017-10-23 21:25:43 -0400

    More test added :fire::fire::fire:

--- test/test.js
+++ test/test.js
@@ -6,14 +6,21 @@ const [should, expect] = [chai.should(), chai.expect];
 const Furler = require('../src/Furler');
 
 describe('Furler Tests Running...', () => {
-	const ly = new Furler();
+	const ly = new Furler('Sia');
 
 	it('Should create a new instance', () => {
 		((typeof Furler).should.equal('function'));
 		expect(ly instanceof Furler).to.equal(true);
 	});
 	it('Should show error message if lyrics is not found', () => {
-		const lrx = new Furler('Sia').Lyrics('Eye of a needle');
-		expect(lrx).to.be.undefined;
+		expect(ly.Lyrics('Eye of a needle')).to.be.undefined;
+	});
+	it('RemoveWhiteSpace method should work properly', ()=>{
+		expect(ly.RemoveWhiteSpace('     test string   ')).to.equal('test string');
+		expect(ly.RemoveWhiteSpace(123)).to.equal('123');
+	});
+	it('Spacer method should work properly', ()=>{
+		expect(ly.Spacer(' #Test String...(),   ')).to.equal('teststring');
+		expect(ly.Spacer(234)).to.equal('234');
 	});
 });
