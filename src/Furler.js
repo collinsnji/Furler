dbe60fe364741aa9f6d65bb0152734f482a74518
collinsnji <collins.geek@gmail.com>
2017-10-23 21:24:19 -0400

    Fixes #9: Broken lyric suggestion due to default artist being Sia

--- src/Furler.js
+++ src/Furler.js
@@ -8,54 +8,81 @@
  * @license MIT License https://collingrimm.me/LICENSE.txt
  */
 
-var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var path = require('path');
 var chalk = require('chalk');
 var { exec } = require('child_process');
 
-var Spacer = function (str) { return str.toString().toLowerCase().match(/[^_\s\W]+/g).join(''); }
-var whiteSpace = function(str){ return str.toString().trim(); }
-var lyricsDir = path.join(path.resolve('.'), 'lyrics');
+var Spacer = function (str) {
+    if (!str) return;
+    return str.toString().toLowerCase().match(/[^_\s\W]+/g).join('');
+}
+var whiteSpace = function (str) { return str.toString().trim(); }
 
 /**
  * Main Furler Class. Can take an optional "artist name" as parameter.
  * @param {String} artist Name of artist. Default => 'Sia'
  */
 class Furler {
-    constructor(artist) {
-        this.artist = artist;
+    constructor(artist) { this.artist = artist; }
+    /**
+     * Remove trailing white-space from string passed as argument. The two functions:
+     * RemoveWhiteSpace and Spacer are similar, but the regex for spacer removes 
+     * every white-space character (not just the trailing ones) and returns the string in lowercase
+     * 
+     * @param {String} str String with white-spaces
+     * @returns {String} Returns string with no white-space / space padding
+     */
+    RemoveWhiteSpace(str) { return str.toString().trim(); }
+    Spacer(str) {
+        if (!str) return;
+        return str.toString().toLowerCase().match(/[^_\s\W]+/g).join('');
     }
     /**
-     * Get the lyrics of a song and save it as a text file in the lyrics directory
-     * TODO: Make the lyrics display on the CLI
+     * Get the lyrics of a song and display it on the terminal
      * 
      * @param {String} song Name of song. Defaults to 'Elastic Heart'
      */
     Lyrics(song) {
-        var LyricURL = `https://www.azlyrics.com/lyrics/${this.artist}/${Spacer(song)}.html`;
-        var googleURL = `https://www.google.com/search?q=site%3Ahttps%3A%2F%2Fwww.metrolyrics.com%2F+${song}+${this.artist}`
-        var finalLyrics = [];
-        //console.log(Spacer(this.artist), Spacer(song));
+        var self = this;
+        var LyricURL = `https://www.azlyrics.com/lyrics/${self.Spacer(self.artist)}/${self.Spacer(song)}.html`;
+        var googleURL = `https://www.google.com/search?q=site%3Ahttps%3A%2F%2Fwww.metrolyrics.com%2F+${song}+${self.artist}`
+        /**
+         * Make request to pull the lyrics
+         */
         request(LyricURL, function (error, response, songData) {
+            var finalLyrics = [];
+            var suggestion = [];
             if (response.statusCode == 404) {
-                // Request the google search
-                request(googleURL, function (error, response, body) {                     
+                /**
+                 * If the lyric is not found, do a Google Search and return the parsed results
+                 */
+                request(googleURL, function (error, response, body) {
                     var $ = cheerio.load(body);
-                    // Get the name of the first result, usually the correct song name
                     var googleResponse = $('.r > a').text();
                     if (googleResponse) {
-                        // Clean title, for artist and song
-                        var googleResponse = googleResponse.replace(/metroLyrics|lyrics|audio/ig, '').split('|')
-                        var suggestion = [];
-                        for(let i = 0; i < googleResponse.length; suggestion.push(googleResponse[i++].split('-')));
-                        console.log(suggestion);
-                        console.log(`${chalk.blue('Did you mean:')} ${chalk.green.bold(`"${whiteSpace(suggestion[0][1])} - ${whiteSpace(suggestion[0][0])}"`)}`);
-                        console.log(`\n${chalk.yellow('Other Suggestions')}`)
-                        
-                        for (let i = 1; i < 3; i++){
-                            console.log(`${suggestion[i][1].trim()} -  ${suggestion[i][0].trim()}`);
+                        /**
+                         *  Parse song title, and artist and return an array
+                         */
+                        var googleResponse = googleResponse.replace(/metroLyrics|lyrics|\.\.\.|video|audio/ig, '').split('|')
+
+                        for (let i = 0; i < googleResponse.length; suggestion.push(googleResponse[i++].split('-')));
+
+                        /**
+                         * Show the results of the parsing and the suggestions
+                         */
+                        console.log(`${chalk.blue('Did you mean:')} ${chalk.green.bold(`"${self.RemoveWhiteSpace(suggestion[0][1])} - ${self.RemoveWhiteSpace(suggestion[0][0])}"`)}`);
+                        console.log(`\n${chalk.yellow('Here are some suggestions')}`)
+
+                        for (let i = 1; i < 10; i++) {
+                            if (suggestion[i] === undefined ||
+                                suggestion[i].length <= 1 ||
+                                suggestion[i].length > 2) {
+                                i++;
+                            }
+                            else {
+                                console.log(`${suggestion[i][1].toString().trim()} -  ${suggestion[i][0].toString().trim()}`);
+                            }
                         }
                     }
                 });
